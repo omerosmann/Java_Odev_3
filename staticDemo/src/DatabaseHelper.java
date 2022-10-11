a9a7555163209300914fd88a76ec3084d1c7bf71
omerosmann <113211889+omerosmann@users.noreply.github.com>
2022-10-11 14:54:53 +0300

    Initial commit

---
+++ staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,22 @@
+
+public class DatabaseHelper {
+
+    public static class Crud {
+
+        public static void Delete() {
+
+        }
+
+        public static void Update() {
+
+        }
+    }
+
+    public static class Connection {
+
+        public static void creatConnection() {
+
+        }
+
+    }
+}
---
+++ staticDemo/src/ProductValidator.java
@@ -0,0 +1,18 @@
+
+public class ProductValidator {
+    
+    static {
+        System.out.println("Static yapıcı blok çalıştı");
+    }
+    
+    
+    public static boolean isValid(Product product){
+        if(product.price > 0 && !product.name.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    
+}
